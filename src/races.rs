56c01e141ef24f01d3964bd04440fd69d4b18137
John William Davis <jwdavisdeveloper@gmail.com>
2024-04-15 13:34:55 -0700

    add a file and begin creating enums I will need

---
+++ src/races.rs
@@ -0,0 +1,17 @@
+pub enum KilometerRaces {
+    Five_k,
+    Ten_k,
+    Fifty_k,
+    One_Hundred_k,
+}
+
+pub enum MileRaces {
+    FiveMile,
+    TenMile,
+    TwentyMile,
+    Marathon,
+    FiftyMile,
+    One_Hundred_Mile,
+    Two_Hundred_Mile,
+    Three_Hundred_Mile,
+}
